4f8b6194556a27048f0f766b778136223c36debb
Nilay Tripathi <nt406@ilab2.cs.rutgers.edu>
2024-11-15 17:29:35 -0500

    Being work on scraping script

---
+++ cf_scrape.py
@@ -0,0 +1,13 @@
+import requests
+import hashlib
+from dotenv import load_dotenv
+import os 
+from datetime import datetime
+
+load_dotenv()
+
+key = os.getenv("KEY")
+secret = os.getenv("SECRET")
+time_stamp = int(datetime.now().timestamp())
+
+print(time_stamp)
