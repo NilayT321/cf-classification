1de20b874ffc24ee8f09c9b488642ceff21ad654
NilayT <ntripathi2003@gmail.com>
2024-11-17 19:59:11 -0500

    Clean up directory and fix scraper.

--- scraping/scraper_v2.py
+++ scraping/scraper_v2.py
@@ -35,19 +35,20 @@ with open("problemsThirdSet.csv", "w", encoding = "utf-8") as f:
             url = f"https://codeforces.com/problemset/page/{page}"
             driver.get(url)
         
+        # Wait for the page to load 
+        time.sleep(8)
+
         # Get the page source 
         source = str(driver.page_source.encode())
         source_parser = BeautifulSoup(source, "html.parser")
-
-        # Wait for the page to load 
-        time.sleep(8)
 
         # Find the table 
         table = source_parser.find('table', attrs = {'class' : 'problems'})
         table = table.find('tbody')
 
         # Loop through all rows except the first, which is the header
-        for row in table.find_all('tr')[1:]:
+        rows = table.find_all("tr")[1:]
+        for row in rows:
             # Get all the cells in the td tags 
             cells = row.find_all('td')
 
@@ -80,7 +81,7 @@ with open("problemsThirdSet.csv", "w", encoding = "utf-8") as f:
             # Many things can go wrong here, so this is wrapped in a try catch block
             try:
                 # Find the link of the problem name and click it 
-                driver.find_element(By.LINK_TEXT, name).click()
+                driver.find_element(By.LINK_TEXT, index).click()
 
                 # Get the HTML and create a new parser for it 
                 subpage_source = str(driver.page_source.encode())
@@ -88,21 +89,23 @@ with open("problemsThirdSet.csv", "w", encoding = "utf-8") as f:
                 # Find the div tag of the problem statement
                 prob_div = subpage_parser.find('div', attrs = {'class' : 'problem-statement'})
             except:
-                # No need to do anything fancy. Just move on 
-                pass 
-            finally:
+                # Go back to the main page if an error occurs
                 driver.back()
+                continue
             
-            # Tenth div in the problem statement has the relevant text. 
-            # This was manually counted and hard coded. 
+            # Second div has the text
             # Of fucking course, this is the only div which doesn't have an id or class
-            prob_div = prob_div.find_all('div')[10]
+            
+            prob_div = prob_div.find_all('div', recursive = False)[1]
 
             prob_statement = clean_output(prob_div.get_text())
-
+# 
             # Finally, write a row to the csv file 
             prob_writer.writerow([index, name, tags, prob_statement, rating])
             print(f"Writing problem {name} successful")
+
+            # Go back
+            driver.back()
         
         print(f"Finished writing {page}")
             
